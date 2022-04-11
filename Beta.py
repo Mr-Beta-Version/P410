4eec993c86a17a7b51f943ba14f86dc8d0a22f05
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2022-04-11 10:25:45 +0600

    Create Beta.py

---
+++ Beta.py
@@ -0,0 +1,27 @@
+import os, platform
+os.system('git pull')
+try:
+    import requests
+except:
+    os.system('python3 -m pip install requests')
+    os.system('python3 -m pip install bs4')
+    os.system('python3 -m pip install futures')
+    os.system('python3 -m pip install machine')
+    os.system('python3 -m pip install mechanize')
+try:
+    import mechanize
+except:
+    os.system('python3 -m pip install mechanize')
+try:
+    import rich
+except:
+    os.system('python3 -m pip install rich')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from p410v1 import bnsbuy
+    bnsbuy()
+elif bit == '32bit':
+    from p410v1 import bnsbuy
+    bnsbuy()
